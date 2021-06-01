545c13c354122e2ae404ba9eea35a6ce7e482984
eliko <elikohon@gmail.com>
2021-06-01 12:11:09 -0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -14,11 +14,11 @@ router.get('/recetaingredientes/:id_receta', getRecetaIngrediente);
 router.get('/imagen/:id_receta', getImageReceta);
 
 router.get('/categorias', getCategorias);
-router.get('/categoria:id_categoria', getCategoria);
+router.get('/categoria/:id_categoria', getCategoria);
 router.get('/categoriareceta/:id_categoria', getCategoriaReceta);
 
 router.get('/ingredientes', getIngredientes);
-router.get('/ingrediente:id_ingrediente', getIngrediente);
+router.get('/ingrediente/:id_ingrediente', getIngrediente);
 router.get('/ingredientesreceta/:id_ingrediente', getIngredienteReceta);
 
 
