3d568bdd5e5ac26097e9c29b67db1118e11bafdb
eliko <elikohon@gmail.com>
2021-06-01 18:18:16 -0300

    Rutas corregidas

--- routes/index.js
+++ routes/index.js
@@ -9,17 +9,21 @@ router.get('/users', getUsers);
 
 router.get('/recetas', getRecetas);
 router.get('/receta/:id_receta', getReceta);
-router.get('/recetacategoria/:id_receta', getRecetaCategoria);
-router.get('/recetaingredientes/:id_receta', getRecetaIngrediente);
-router.get('/imagen/:id_receta', getImageReceta);
+router.get('/receta/:id_receta/categorias', getRecetaCategoria);
+router.get('/receta/:id_receta/categorias/:id_categoria', getCategoria);
+router.get('/receta/:id_receta/ingredientes', getRecetaIngrediente);
+router.get('/receta/:id_receta/ingredientes/:id_ingrediente', getIngrediente);
+router.get('/receta/:id_receta/imagen', getImageReceta);
 
 router.get('/categorias', getCategorias);
 router.get('/categoria/:id_categoria', getCategoria);
-router.get('/categoriareceta/:id_categoria', getCategoriaReceta);
+router.get('/categoria/:id_categoria/recetas', getCategoriaReceta);
+router.get('/categoria/:id_categoria/recetas/:id_receta', getReceta);
 
 router.get('/ingredientes', getIngredientes);
 router.get('/ingrediente/:id_ingrediente', getIngrediente);
-router.get('/ingredientesreceta/:id_ingrediente', getIngredienteReceta);
+router.get('/ingrediente/:id_ingrediente/receta', getIngredienteReceta);
+router.get('/ingrediente/:id_ingrediente/receta/:id_receta', getReceta);
 
 
 
