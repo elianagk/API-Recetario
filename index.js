92ce4e58c5e3e24aa49c97414c89c8b5e6f54bca
eliko <elikohon@gmail.com>
2021-06-18 11:02:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const PORT = process.env.PORT || 5000
 const app = express();
-const cors = require('cors');
 
+app.use(cors())
 app.use(express.json());
 app.use(require('./routes/index'));
-app.use(cors())
 app.listen(PORT);
 
 // Swagger
